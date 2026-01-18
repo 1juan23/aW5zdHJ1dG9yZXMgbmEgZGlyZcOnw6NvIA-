07297480c3cf8b3910d747fbe2109b8a6bf85cac
Juan H. <63318731+1juan23@users.noreply.github.com>
2026-01-17 22:24:48 -0300

    instrutoresnadirecao.com.br
    instrutoresnadirecao.com.br

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: '/icon-192x192.png', // Make sure this icon exists or use a default one
+      badge: '/badge-72x72.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Ver detalhes', icon: '/checkmark.png'},
+        {action: 'close', title: 'Fechar', icon: '/xmark.png'},
+      ]
+    };
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('https://instrutoresnadirecao.com.br/mensagens') // Redirect to messages by default
+  );
+});
